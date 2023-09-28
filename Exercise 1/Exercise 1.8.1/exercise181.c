2d0ed2458b4d0165726cb3b99f5acdf705831c8a
mihirgandecha <mihirgandecha@outlook.com>
2023-09-28 11:38:45 +0100

    today

--- Exercise 1/Exercise 1.8.1/exercise181.c
+++ Exercise 1/Exercise 1.8.1/exercise181.c
@@ -1,29 +1,10 @@
 #include <stdio.h>
 
-int main(void)
+int main(void){
 
-{
-    int a, b, c;
+    int total_num, 
 
-    printf("Please enter three values: ");
-    if(scanf("%i %i %i", &a, &b, &c) != 3)
-        printf("Fail. Please input three integers bro.");
 
-        return 1;
-    
-    int max;
 
-    if(a > b && a > c){
-        max = a;
-    }
-
-    if(b > a && b > c){
-        max = b;
-    }
-
-    else{
-        max = c;
-    }
-    printf("The biggest number out of the three is%i\n", max);
     return 0;
 }
