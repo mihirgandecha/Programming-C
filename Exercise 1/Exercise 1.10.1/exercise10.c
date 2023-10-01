143b7953aaa1864d858436908dcb8c1bf2e9b7a6
mihirgandecha <mihirgandecha@outlook.com>
2023-10-01 17:52:06 +0100

    twas a nested loop!

--- Exercise 1/Exercise 1.10.1/exercise10.c
+++ Exercise 1/Exercise 1.10.1/exercise10.c
@@ -1,45 +1,31 @@
 #include <stdio.h>
-//STEP 1
-#define A 0
-#define C 0
-#define M 0
-#define SEED 0
 
 int main(void){
+    int A = 0;
+    int C = 0;
+    int M = 0;
+    int SEED = 0;
 
-//STEP 2: Determine the period (number of iterations it takes for sequence to start repeating itself):
+    int found_repeat = 0;
 
-int new_A += A; 
-int new_C += C;
-int new_M += M;
+    printf("What numbers would you like to input into the LCG loop? A =  C =  M =");
+    scanf("%i%i%i", &A, &C, &M);
 
-printf("What numbers would you like to input into the LCG loop? A =  C =  M =  ");
-scanf("%i%i%i", &new_A, &new_C, &new_M);
+    int current_seed = SEED;
 
-//WHEN CONTINUE INITIALISE FOR LOOP AND PUT IF STATEMENT!
+    for(int i = 0; i < M && !found_repeat; i++){ //initialising counter i
+        current_seed = (A * current_seed + C) % M;
 
-    for(i = 0; i < 10; i++){
-        seed = (A * seed + C) % M; //sequence length MAX is same as M
+        int check_repeats_seed = current_seed; 
+        for(int j = i + 1; j < M && !found_repeat; j++){ 
+            check_repeats_seed = (A * check_repeats_seed + C) % M;
 
-
-int current_seed = SEED,
-int period = 0;
-int initial, current;
-
-initial = (A * current_seed + C) % M;
-current = initial;
-
-
-
-
-
-
-    for(i = 0; i < 10; i++){
-        seed = (A * seed + C) % M; //sequence length MAX is same as M
-
-    printf("%i\n", seed);
+            if (current_seed == check_repeats_seed) {
+                found_repeat = 1;
+                printf("The sequence starts repeating after %d iterations.\n", j - i);
+            }
+        }
     }
-
 
     return 0;
 }
