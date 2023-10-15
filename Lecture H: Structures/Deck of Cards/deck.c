6588a799d3fc9f2f9190753b41f86c460d58dc81
mihirgandecha <mihirgandecha@outlook.com>
2023-10-15 23:24:22 +0100

    decky

--- Lecture H: Structures/Deck of Cards/deck.c
+++ Lecture H: Structures/Deck of Cards/deck.c
@@ -25,7 +25,7 @@ Here we are calling the data type suits
 Note we can typedef straight away!*/
 
 
-typedef enum {hearts, suits, spades, clubs} suits;
+typedef enum {hearts, diamonds, spades, clubs} suit;
 
 /* 3 Functions:
     1. Initialise the deck of cards. SET IT UP! To do this we have to create a structure.
@@ -52,7 +52,7 @@ void init_deck(card d[DECK]);
 void print_deck(card d[DECK], int n);
 void test(void);
 
-int main void
+int main (void)
 {
 
     card d[DECK];
@@ -65,11 +65,15 @@ int main void
     return 0;
 }
 
-void init_deck(card d[DECK])
+void init_deck(card d[DECK]) //flowchart
 {
 
     for(int i = 0; i < DECK; i++){
-        d[i].pips = (1 % PERSUIT) + 1;
+        //first we are assigning numbers
+        d[i].pips = (i % PERSUIT) + 1; //first we are accessing i-th element of array of type card
+        //we are assigning (1 % PERSUIT) + 1
+
+        //here we are assigning suit
         switch (i / PERSUIT){
             case hearts: d[i].st = hearts;
             break;
@@ -88,3 +92,79 @@ void init_deck(card d[DECK])
         }
     }
 }
+
+//REMEMBER ARRAY IS NOT COPIED!
+//Cards out on table. Take 2 and swap them
+//keep shuffling fixed number of times
+
+void shuffle_deck(card d[DECK])
+{
+    for(int i = 0; i < SHUFFLE*DECK; i++){ 
+        int n1 = rand()%DECK; 
+        int n2 = rand()%DECK; //choose 2 numbers randomly (cell of array). % ensures it is within range of valid card positions
+        //make sure you create a copy!!!
+        card c = d[n1]; d[n1] = d[n2]; d[n2] = c;
+        //c is a temporary new card, and is used to store card at position n1 
+        // d[n1] = d[n2] means that the card at position n1 is replaced with card at position n2 
+        //now c is the replaced card
+    }
+}
+
+//print deck - give me array of cards - i want to print out the first n cards
+void print_deck(card d[DECK], int n) {
+    for (int i = 0; i < n; i++) {
+        switch (d[i].pips) {
+            case 11:
+                printf("Jack"); //if pip is 11 then print Jack
+                break; //replace with string
+            case 12:
+                printf("Queen");
+                break;
+            case 13:
+                printf("King");
+                break;
+            default: // for any values of pips, print this:
+                printf("%2i", d[i].pips);
+        }
+
+        switch (d[i].st) {
+            case hearts:
+                printf(" of Hearts\n");
+                break;
+            case diamonds:
+                printf(" of Diamonds\n");
+                break;
+            case spades:
+                printf(" of Spades\n");
+                break;
+            default:
+                printf(" of Clubs\n");
+        }
+    }
+    printf("\n");
+}
+
+void test(void) {
+
+    int n = 0;
+    card d[DECK];
+    init_deck(d); //Direct assignment
+
+    card c = {hearts, 10}; //1st element initialised correctly
+    assert(d[9].pips == c.pips);
+    assert(d[9].st==c.st);
+    for(int i=0;i<1000;i++){
+        shuffle_deck(d); //Happens 1 time in52 ?
+        if((d[0].st==c.st)&&(d[0].pips==c.pips)){
+            n++;
+        }
+    } // Is this a reasonable test ?
+    assert((n>10)&&(n<30));
+}
+
+
+
+
+
+
+
