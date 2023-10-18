62dc3043be99c7d48a155c1865cb7796a346feb5
mihirgandecha <mihirgandecha@outlook.com>
2023-10-18 12:24:32 +0100

    tod

--- Assignment-Minesweeper/ms.c
+++ Assignment-Minesweeper/ms.c
@@ -2,6 +2,9 @@
 
 // Maybe some of your own function prototypes here
 
+//order of function is always same in drv.c - functionfor this?
+//print board function?
+
 /*board solve_board(board b)
 {
 }*/
@@ -10,15 +13,18 @@
 {
 }*/
 
-/*bool syntax_check(unsigned totmines, unsigned width, unsigned height, char inp[MAXSQ*MAXSQ+1])
+// Make sure number of characters in string == width*height
+// Ensure only characters are from the set:   0123456789?X
+// Ensure mines in string <= totmines
+bool syntax_check(unsigned totmines, unsigned width, unsigned height, char inp[MAXSQ*MAXSQ+1])
 {
-}*/
+}
 
 // Returns a board based on the totmines, width, height and grid (passed as a string)
 board make_board(int totmines, int width, int height, char inp[MAXSQ*MAXSQ+1])
 {
     //Initialise board objects.
-    board new_board;
+    board b;
     b.w = width;
     b.h = height;
     b.totmines = totmines;
@@ -30,7 +36,7 @@ board make_board(int totmines, int width, int height, char inp[MAXSQ*MAXSQ+1])
     }
   }
 
-  return new_board;
+  return b;
 }
 
 /*void test(void)
