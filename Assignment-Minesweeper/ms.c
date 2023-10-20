70b02a9715cbb253145e891ba94553228829f382
mihirgandecha <mihirgandecha@outlook.com>
2023-10-20 03:16:10 +0100

    new2

--- Assignment-Minesweeper/ms.c
+++ Assignment-Minesweeper/ms.c
@@ -4,8 +4,7 @@
 
 // Maybe some of your own function prototypes here
 
-//function for initialising?
-int string_check(char* inp, int max_len);
+bool string_check(char* inp, int max_len);
 
 int count_mines(char* inp, int max_len);
 int count_unknown(char* inp, int max_len);
@@ -19,22 +18,28 @@ board applyRuleTwo(board b);
 bool apply_rules(board* b, bool* board_changed);
 
 //Function for checking string is within rules
-int string_check(char* inp, int max_len){
+bool string_check(char* inp, int max_len){
   char rules[] = "012345678?X";
   int rules_len = strlen(rules);
+  rules_len++;
+  //Flag for checking if character is allowed
+  bool char_allowed;
 
   for (int i = 0; i < max_len; i++){
-    int rule_broken = 0;
+    //Assuming initially character is not allowed
+    char_allowed = false;
     for (int j = 0; j < rules_len; j++){
       if(inp[i] == rules[j]) {
-        rule_broken = 1;
+        char_allowed = true;
       }
     }
-    if (!rule_broken){
-      return 0;
+    if (!char_allowed){
+      //Return false early if character is not allowed
+      return false;
     }
   }
-  return 1;
+  //Return true if all characters are allowed
+  return true;
 }
 
 //Function for counting mines in grid
@@ -114,10 +119,11 @@ int mines_around_cell(board b, int x, int y){
 board applyRuleOne(board b){
   for (int j = 0; j < b.h; j++){
     for (int i = 0; i < b.w; i++){
+      //Check if cell is unknown
       if (b.grid[i][j] == UNK){
         int mines = mines_around_cell(b, i, j);
-        if (mines == b.totmines){
-
+        //Check for mines around cell
+        if (mines > 0){
           b.grid[i][j] = int_to_char(mines);
         }
       }
@@ -136,20 +142,23 @@ board applyRuleTwo(board b) {
         int mines = mines_around_cell(b, i, j);
         int unknowns = unknown_around_cell(b, i, j);
         int total = char_to_int(current_cell);
-
+      
+      //Is the number of mines around cell equal to the number of unknowns around cell?
       if((total - mines) == unknowns){
-        
+        //Creating a 3 x 3 grid around cell
         for (int point_i = -1; point_i <= 1; point_i++){
           for (int point_j = -1; point_j <= 1; point_j++){
             int newpoint_i = i + point_i; 
             int newpoint_j = j + point_j;
-
+            
+            //Check if newpoint_i and newpoint_j is within board width and height
             if(
               (newpoint_j >= 0 && newpoint_j < b.h) &&  
               (newpoint_i >= 0 && newpoint_i < b.w) &&
               b.grid[newpoint_i][newpoint_j] == UNK){
                 
-                b.grid[newpoint_i][newpoint_j] = MINE;
+              //Set unknowns '?' to mines 'X'
+              b.grid[newpoint_i][newpoint_j] = MINE;
               }      
             }
           }
@@ -180,6 +189,7 @@ bool apply_rules(board* b, bool* board_changed) {
   return *board_changed; // return the status of board_changed
 }
 
+//ms.h function
 
 board solve_board(board b){
 
@@ -194,11 +204,20 @@ board solve_board(board b){
 }
 
 void board2str(char s[MAXSQ*MAXSQ+1], board b){
-  for (int i = 0; i < b.h; i++){
-    for(int j = 0; j < b.w; j++){
-      s[i * b.w + j] = b.grid[i][j];
+
+  
+  int index = 0;
+  int max_len = strlen(s);
+  max_len++;
+
+  for (int i = 0; i < b.h && (index < max_len); i++){
+    //Iterating index to ensure we are including null terminator
+    for(int j = 0; j < b.w && (index < max_len); j++, index++){
+      s[index] = b.grid[i][j];
     }
   }
+  //Adding the null terminator to end of string
+  s[index] = '\0';
 }
 
 
@@ -209,53 +228,67 @@ if ((width > MAXSQ) || (height > MAXSQ)){
 }
 
 //Where width and height = 0
-if ((width == 0) && (height == 0)){
-  return max_len == 0;
+if ((width == 0) || (height == 0)){
+  return false;
 }
 
 int max_len = strlen(inp);
+max_len++;
 int board_len = width * height;
 
 // Make sure number of characters in string == width*height
-if (max_len != (board_len + 1)){ //check +1
+if (max_len != board_len){ 
   return false;
 }
 
 // Ensure only characters are from the set:   0123456789?X
-return string_check(inp, max_len);
+if (!string_check(inp, max_len)){
+  return false;
+}
   
 // Ensure mines in string <= totmines
-int mine_string = count_mines(inp, max_len -1);
+int mine_string = count_mines(inp, max_len);
+int unknown_string = count_unknown(inp, max_len);
 
 if ((unsigned)mine_string > totmines){
   return false;
-}
+  }
 
+if (unknown_string > 0 && (unsigned)(mine_string + unknown_string) > totmines){
+    return false;
+  }
 
 return true;
 }
 
 // Returns a board based on the totmines, width, height and grid (passed as a string)
 board make_board(int totmines, int width, int height, char inp[MAXSQ*MAXSQ+1]){
-    //Initialise board objects.
-    board b;
-    b.w = width;
-    b.h = height;
-    b.totmines = totmines;
+  //Initialise board objects.
+  board b;
+  b.w = width;
+  b.h = height;
+  b.totmines = totmines;
 
-    //Initialise board grid (1D Arr -> 2D Arr)
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        b.grid[i][j] = (int)inp[i * width + j];
+  //Initialise board grid (1D Arr -> 2D Arr)
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      b.grid[i][j] = (int)inp[i * width + j];
       }
     }
+
   return b;
   }
 
 void test(void){
-  
-  //string_check
-  assert(string_check("012345?X6789", 11) == 1);
-  assert(string_check("12345abX?8" 11) == 0);
-  
+  char test1[] = "123?456789";
+  char test2[] = "XXXXXXXXX";
+  char test3[] = "??????????";
+
+  assert(count_mines(test1, 11) == 0);
+  assert(count_mines(test2, 10) == 9);
+  assert(count_mines(test3, 11) == 0);
+
+  assert(count_unknown(test1, 11) == 1);
+  assert(count_unknown(test2, 10) == 0);
+  assert(count_unknown(test3, 11) == 10);
 }
