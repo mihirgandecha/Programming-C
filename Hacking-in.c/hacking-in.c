e1edd0912c90334a97a2bf05af76d1287f3bf623
mihirgandecha <mihirgandecha@outlook.com>
2023-10-23 12:49:03 +0100

    hacking

---
+++ Hacking-in.c/hacking-in.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*
+
+GDB:
+
+bt - command for ...
+
+list - see the code
+list main
+list <function name>
+
+disas - Dump of assembler code for function variable
+
+stack - remember where you are, then recall
+
+break or b
+
+i r - see register
+
+echo - e ""  disasm
+
+System Calls Manual - execve
+
+Where the buffer is?
+
+Binary search - try and see?
+
+Learn change programme using spawn shell in gdb
+
+Read hacking papers
+
+backdoor bash- injecting a backdoor (DONT DO AT UNI)
+
+*/
