453b6128fd943ba58f8a6d897d452bb5469649c5
mihirgandecha <mihirgandecha@outlook.com>
2023-11-14 10:55:25 +0000

    tues

--- 8q/8q.c
+++ 8q/8q.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]){
     int n = MAX_BOARD;
-    board t;
+    board parent_board;
     bool verbose = false;
 
     if (argc < 2 || argc > 3){
@@ -19,8 +19,8 @@ int main(int argc, char *argv[]){
     if (argc == 3 && strcmp(argv[2], "verbose") == 0){
         verbose = true;
     }
-    printf("%2d\n",parentBoard(&t, n));
-    
+
+    *parent_board->queen_count = n; 
     
     //For 3x3 board there should be 9 childboards
     // int childb_number = n * n;
@@ -32,8 +32,35 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
+char isSafecr(board childBoard, *parentBoard, int n, int queen_count){
 
-int parentBoard(board *b, int n){
+    //For a 4x4 board:
+    assert(isSafecr(childBoard1, *parentBoard, int 4, int 4) == 2031, count_child == 1);
+    assert(childBoard1.quenn_count == 4);
+    assert(childBoard1.empty_count == 12);
+    //Squares to be filled = n*n, make sure empty+queen = n*n
+    assert(childBoard1.queen_count + childBoard1.empty_count = n*n);
+
+
+
+    int f[0] = *parentBoard;
+    //Make into assert test?
+    *parentBoard.nposib = n*n;
+    *parentBoard.quenn_count = n;
+    *parentBoard.empty_count = *parentBoard.nposib - *parentBoard.queen_count;
+    
+    //For row, first placement:
+    
+
+
+
+    //Output: childBoard -> increase counter, output solution of where Q is along j as 1-D Arr
+
+    return 0;
+}
+
+
+void emptyBoard(board *b, int n){
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
             b->chessboard[i][j] = EMPTY;
@@ -42,3 +69,11 @@ int parentBoard(board *b, int n){
     
 }
 
+char printBoard(board *c, int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            printf("%c", c->chessboard);
+        }
+    }
+    return 0;
+}
--- 8q/8q.h
+++ 8q/8q.h
@@ -6,10 +6,14 @@
 #include <stdlib.h>
 
 #define MAX_BOARD 10
-#define EMPTY -1
+#define EMPTY 'X'
+#define QUEEN 'Q'
 
 typedef struct board {
-   int chessboard[MAX_BOARD][MAX_BOARD];
+   char chessboard[MAX_BOARD][MAX_BOARD];
+   int queen_count;
+   int empty_count;
+   int nposib;
 } board;
 
 int parentBoard(board *b, int n);
