efe93ecb0926c9c44b86c512e58ad75d328d2d13
mihirgandecha <mihirgandecha@outlook.com>
2023-11-15 13:24:49 +0000

    wed

--- 8q/8q.c
+++ 8q/8q.c
@@ -1,12 +1,15 @@
 #include "8q.h"
 
 int main(int argc, char *argv[]){
+    //Move to solveBoard after
     int n = MAX_BOARD;
     board parent_board;
+    queen_array four;
     bool verbose = false;
-
-    if (argc < 2 || argc > 3){
-        fprintf(stderr, "Usage: %s <size> [verbose]\n", argv[0]);
+    
+    //Start of taking cmnd line arguement
+    if (argc != 2){
+        fprintf(stderr, "Usage: %s <fileout>\n", argv[0]);
         return EXIT_FAILURE;
     }
     
@@ -19,87 +22,119 @@ int main(int argc, char *argv[]){
     if (argc == 3 && strcmp(argv[2], "verbose") == 0){
         verbose = true;
     }
+    //end of cmnd line arguement
 
-    *parent_board->queen_count = n; 
+    //Initialise empty board
+    emptyBoard(&parent_board, n);
+    //Check in-bound
+    in_bound(&parent_board, n);
+
+    //Initalise queen array structure for board
+    q_arr(&four, n);
+    // Print the Q-Current Pos - convert into a function and remove at end
+    printf("\nCurrent Q-Positions: \n");
     
-    //For 3x3 board there should be 9 childboards
-    // int childb_number = n * n;
-    // int queens = n;
+    
+    for (int i = 0; i < n; i++) {
+        printf("%c at index %d is positioned at (%d, %d)\n", four.queen[i], four.q_index[i], four.row[i], four.col[i]);
+    }
 
-    // board solution[n] = {
-    //     {}
-    // }
+
+    printf("\n");
+    //print empty board
+    printBoard(&parent_board, n);
+    printf("\n");
+
+    //Now we have empty board of nxn, and corresponding structure of q-arr if 4 queens all indexed
+
+    //Initialise the array list of structures for 4(N) board to solve
+    board solve_list[MAX_BOARD];
+    //Set f = 0 to parentBoard and print
+    int f = 0;
+    solve_list[f] = parent_board; 
+    //print f = 0 to test array list of structure - for f=0:
+    printBoard(&solve_list[f], n);
+
+    //solve for f = 1 (placing 1 queen)
+    f++;
+    four.row[0] = 0;
+    four.row[0] = 0; 
+
+    test();
     return 0;
-}
-
-
-char isSafecr(board childBoard, *parentBoard, int n, int queen_count){
-
-    //For a 4x4 board:
-    assert(isSafecr(childBoard1, *parentBoard, int 4, int 4) == 2031, count_child == 1);
-    assert(childBoard1.quenn_count == 4);
-    assert(childBoard1.empty_count == 12);
-    //Squares to be filled = n*n, make sure empty+queen = n*n
-    assert(childBoard1.queen_count + childBoard1.empty_count = n*n);
-
-
-
-    int f[0] = *parentBoard;
-    //Make into assert test?
-    *parentBoard.nposib = n*n;
-    *parentBoard.quenn_count = n;
-    *parentBoard.empty_count = *parentBoard.nposib - *parentBoard.queen_count;
-    
-    //make 1D-array of Q for 4x4
-    //int queen_array[n] = {}; where inside is only Q
-    char qArr_test(int *board.queen_count){
-    
-    if (int queen_count = 4){
-        q_arr[4] = {'Q', 'Q', 'Q', 'Q'};
-        return 0;
-    }
-    else{
-        return 1;
-    }
-    }
-    //Rewritten to test for 4x4:
-    assert(char qArr_test(4) == char queen_array[4] = {'Q', 'Q', 'Q', 'Q'});
-
-    //Create queen_array function 
-    
-
-    struct 
-    for(int i=0; i<n; i++){
-
-    }
-
-    //After syntax check on array: 
-
-    //For row, first Q1 placement:
-    
-
-
-
-    //Output: childBoard -> increase counter, output solution of where Q is along j as 1-D Arr
 
     return 0;
 }
 
+    
+void q_arr(queen_array *t, int board_size){
+    
+    t->board_size = board_size;
+    
+    for (int i = 0; i < board_size; i++) {
+        t->queen[i] = QUEEN;      
+        t->q_index[i] = i + 1;  
+        t->row[i] = OUT_BOUNDS;
+        t->col[i] = OUT_BOUNDS;
+    }
+}
 
-void emptyBoard(board *b, int n){
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
+bool in_bound(board *b, int board_size){
+    int row, col = board_size -1;
+    if (row < 0 || col < 0){
+        return false;
+    }
+    if(row > board_size || col > board_size){
+        return false;
+    }
+    return true;
+}
+    
+void emptyBoard(board *b, int board_size){
+    for (int i = 0; i < board_size; i++){
+        for (int j = 0; j < board_size; j++){
             b->chessboard[i][j] = EMPTY;
         }
     }
+}
+
+void printBoard(board *b, int board_size){
+for (int i = 0; i < board_size; i++){
+        for (int j = 0; j < board_size; j++){
+            printf("%c", b->chessboard[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// char isSafecr(board childBoard, *parentBoard, int n, int queen_count){
+
+//     //For a 4x4 board:
+//     assert(isSafecr(childBoard1, *parentBoard, int 4, int 4) == 2031, count_child == 1);
+//     assert(childBoard1.quenn_count == 4);
+//     assert(childBoard1.empty_count == 12);
+//     //Squares to be filled = n*n, make sure empty+queen = n*n
+//     assert(childBoard1.queen_count + childBoard1.empty_count = n*n);
+
+
+
+//     int f[0] = *parentBoard;
+//     //Make into assert test?
+//     *parentBoard.nposib = n*n;
+//     *parentBoard.quenn_count = n;
+//     *parentBoard.empty_count = *parentBoard.nposib - *parentBoard.queen_count;
+// }
+void test(void){
+    queen_array six;
+    q_arr(&six, 6);
+    
+    assert(six.board_size == 6);
+    assert(six.q_index[0] == 1 && six.q_index[5] == 6);
     
 }
 
-char printBoard(board *c, int n){
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            printf("%c", c->chessboard);
-        }
-    }
-    return 0;
-}
+//Assert testing - works sometimes but not always (writing first)
+//Going from me writing 4x4 -> for any N up to 10
+//Test for print? I just did out of bound function
+//#define - should I do for Q[0] when 0 is hash defined to be INITIAL
+//Comments - right now writing comments as a guide to myself, however how can I follow house style as 'effective' as i am writing?
--- 8q/8q.h
+++ 8q/8q.h
@@ -8,14 +8,22 @@
 #define MAX_BOARD 10
 #define EMPTY 'X'
 #define QUEEN 'Q'
+#define OUT_BOUNDS -1
+
+typedef struct queen_array {
+    int board_size;
+    char queen[MAX_BOARD];
+    int q_index[MAX_BOARD];
+    int row[MAX_BOARD];
+    int col[MAX_BOARD];
+} queen_array;
 
 typedef struct board {
    char chessboard[MAX_BOARD][MAX_BOARD];
-   int queen_count;
-   int empty_count;
-   int nposib;
-   char queen_array[n] = {};
 } board;
 
-int parentBoard(board *b, int n);
+void q_arr(queen_array *t, int board_size);
+void emptyBoard(board *b, int board_size);
+void printBoard(board *b, int board_size);
+bool in_bound(board *b, int board_size);
 void test(void);
