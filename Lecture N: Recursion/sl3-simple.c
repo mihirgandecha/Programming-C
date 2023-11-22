670cd6c1ceabb2742c096524b27c6a861f43a089
mihirgandecha <mihirgandecha@outlook.com>
2023-11-22 15:35:08 +0000

    22nov

--- Lecture N: Recursion/sl3-simple.c
+++ Lecture N: Recursion/sl3-simple.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-#define SWAP(A,B) {char temp}; temp = A; B = temp
+#define SWAP(A,B) {char temp; temp = A; A = B; B = temp;}
+
+void strrev(char* s, int n);
+
+int main(void){
+
+    char str[] = "Hello World!";
+    strrev(str, strlen(str));
+    printf("%s\n", str);
+    return 0;
+}
+
+void strrev(char* s, int n)
+{
+    for (int i = 0, j = (n - 1); i < j; i++, j--){
+        SWAP(s[i], s[j]);
+    }
+}
