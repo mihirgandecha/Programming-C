e00d08eb2717d5345f22626828510ddeaa71c537
mihirgandecha <mihirgandecha@outlook.com>
2023-11-29 16:52:36 +0000

    10.1

--- Exercise 10/Exercise 10.1/bst.c
+++ Exercise 10/Exercise 10.1/bst.c
@@ -7,8 +7,8 @@
 #define STRSIZE 5000
 
 struct node {
-    char c;
-    struct node* left;
+    char c; //i.e. 'A' in line 23 for root node (1-byte)
+    struct node* left; //pointer to another node structure (4 or 8 depending on if 32 or 64 bit)
     struct node* right;
 };
 
@@ -21,13 +21,13 @@ char* PrintTree(Node* t);
 int main(void) {
     char c; //garbage value
     Node* head = MakeNode('A'); //Created pointer node head. Using x/1d shows ascii 65 representing 'A' 
-    Node* n;
+    Node* n; //Creates node n, with char being set to 0 through calloc
 
-    srand(time(NULL));
+    srand(time(NULL)); //Such that every time rand is called not same number. Either 0 or 1 (L or R)
 
-    for (c = 'B'; c < 'G'; c++) {
-        n = MakeNode(c);
-        InsertRandom(head, n);
+    for (c = 'B'; c < 'G'; c++) { //c is set to first child, condition is until 'F' node is created, increment number of random node being created 
+        n = MakeNode(c); //So instead of n being 0, char is passed so that struct node includes characters 'b' -> 'f'
+        InsertRandom(head, n); //input parent and child
     }
 
     printf("%s\n", PrintTree(head));
@@ -35,17 +35,18 @@ int main(void) {
     return 0;
 }
 
+//If NULL, means no node on the left/right
 Node* MakeNode(char c) {
-    Node* n = (Node*)calloc(1, sizeof(Node));
-    assert(n != NULL);
-    n->c = c;
+    Node* n = (Node*)calloc(1, sizeof(Node)); //Creates memory for struct Node (so that point left right is used too) space for every node created, setting all bits of mem block to 0 
+    assert(n != NULL); //Check that calloc worked and memory has been allocated, use p *n
+    n->c = c; //storing whatever is in n to the structure (storing the character). So we allocated memory, and now storing 'A' in the structure.
     return n;
 }
-
-void InsertRandom(Node* t, Node* n) {
+//understand what is causing left/right to be set to NULL
+void InsertRandom(Node* t, Node* n) { //input is parent (where the node is inserted) and child (the node to be inserted), such that loop includes child 'B' -> 'F' (only char is changing in struct so far)
     if ((rand() % 2) == 0) { /* Left */
         if (t->left == NULL) {
-            t->left = n;
+            t->left = n; //want to keep calling this function until t-left/right == NULL
         } else {
             InsertRandom(t->left, n);
         }
