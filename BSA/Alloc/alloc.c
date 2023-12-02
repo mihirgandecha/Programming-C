89865607288991010bdd0181c1445f40171f36b4
mihirgandecha <mihirgandecha@outlook.com>
2023-12-02 16:29:38 +0000

    4:30pm sat labs, wrote set, free maxindex function

--- BSA/Alloc/alloc.c
+++ BSA/Alloc/alloc.c
@@ -2,7 +2,7 @@
 
 bsa* bsa_init(void){
     bsa* buildBSA = (bsa*) calloc(1, sizeof(bsa)); //created bsaCOL
-    buildBSA->array = calloc(BSA_ROWS, sizeof(BSA_Col));
+    buildBSA->array = calloc(BSA_ROWS, sizeof(BSA_Col)); //do i need if (build BSA != NULL?)
 
     return buildBSA;
 }
@@ -10,18 +10,41 @@ bsa* bsa_init(void){
 // Set element at index indx (2^k) with value d i.e. b[i] = d;
 // May require an allocation if it's the first element in that row
 bool bsa_set(bsa* b, int indx, int d){
+    //Check that b has been initialised first (it needs to be passed through to bsa_init first). if statement for if not null, call bsa_init? <- check driver.c
+    if (b == NULL || b->array == NULL){
+        return false;
+    }
     //first calculating kth row given the index 
     int k = 0;
-    kth_row(indx, &k);
-    
-    if (k < BSA_ROWS){
-        return true;
+    int kthRow = kth_row(indx, &k);
+
+    //Check that kth row is in bound, false if 30 or higher
+    if (kthRow >= BSA_ROWS){
+        return false;
     }
-    
-    if (b->array != NULL){
-        index_end(d);
+
+    //might need to redo how kth_row works as this calls again (more memory) - calculating array row size, given index
+    //example: row(k) = 2; index-6, d=10
+    int kRowStart = index_start(kth_row); //output 3
+    int kRowEnd = index_end(kth_row); //output 6
+    //row size should be 4
+    int rowSize = (kRowEnd - kRowStart) - 1; //function?
+
+    //check if row (2nd) at k has been created
+    if (b->array[k].positionIndex == NULL){
+        //create the row with the required size
+        b->array[k].positionIndex = malloc(sizeof(int) * rowSize);
+        //check this works^
+        if (b->array[k].positionIndex == NULL){
+            return false;
+        }
+        //initialise the row with default values 'X'? or 0?
+        for (int i = 0; i < rowSize; i++){
+            b->array[k].positionIndex = 0;
+        }
     }
-    return false;
+    //set value d into the created row
+    b->array[k].positionIndex[indx - kRowStart] = d;
 }
 
 int kth_row(int index, int *k){
@@ -43,8 +66,19 @@ int index_start(int k){
     if (k == 0){
         return 0;
     }
-    long result = 1L << k; 
-    return result - 1;
+    return (1L << k) - 1;
+}
+
+void testIstart(void){
+    assert(index_start(0) == 0);
+    assert(index_start(1) == 1);
+    assert(index_start(2) == 3);
+    assert(index_start(3) == 7);
+    assert(index_start(4) == 15);
+    assert(index_start(5) == 31);
+    assert(index_start(6) == 63);
+    assert(index_start(BSA_ROWS - 2) == 268435455); //row 28
+    assert(index_start(BSA_ROWS - 1) == 536870911); //last row 29
 }
 
 int index_end(int k){
@@ -54,51 +88,40 @@ int index_end(int k){
     return (1L << (k + 1)) - 2;
 }
 
+void testIend(void){
+    assert(index_end(0) == 0);
+    assert(index_end(1) == 2);
+    assert(index_end(2) == 6);
+    assert(index_end(7) == 254);
+    assert(index_end(8) == 510);
+    assert(index_end(9) == 1022);
+    assert(index_end(10) == 2046);
+    assert(index_end(11) == 4094);
+    assert(index_end(BSA_ROWS - 1) == 1073741822);
+}
+
 void test_krow(void){
     int k;
-
     k = 0;
     assert(kth_row(0, &k) == 0);
-
     k = 0;
     assert(kth_row(1, &k) == 1);
-
     k = 0;
     assert(kth_row(2, &k) == 1);
-    
     k = 0;
-    assert(kth_row(3, &k) == 2); //works now
-    
+    assert(kth_row(3, &k) == 2);//works now
     k = 0;
-    assert(kth_row(6, &k) == 2); //works now
-    
+    assert(kth_row(6, &k) == 2); 
     k = 0;
-    assert(kth_row(7, &k) == 3); //works now
-
+    assert(kth_row(7, &k) == 3); 
     k = 0;
     assert(kth_row(14, &k) == 3);
-
-    k = 0;
-    assert(kth_row(15, &k) == 4);
-    
-    k = 0;
-    assert(kth_row(17, &k) == 4);
-    
     k = 0;
     assert(kth_row(30, &k) == 4); 
-
-    k = 0;
-    assert(kth_row(32, &k) == 5); 
-
-    k = 0;
-    assert(kth_row(45, &k) == 5);
-
-    k = 0;
-    assert(kth_row(63, &k) == 6);
-
     k = 0;
     assert(kth_row(128, &k) == 7);
 }
+
 // Return pointer to data at element b[i]
 // or NULL if element is unset, or part of a row that hasn't been allocated.
 int* bsa_get(bsa* b, int indx){
@@ -123,16 +146,31 @@ bool bsa_delete(bsa* b, int indx){
 // Returns maximum index written to so far or
 // -1 if no cells have been written to yet
 int bsa_maxindex(bsa* b){
-    return false;
-    if (b->array != NULL){
-        printf("hi");
+    //base case (-1): does b->array.positionIndex == NULL? return
+    if (b->array->positionIndex == NULL){
+        return;
     }
-    return true;
+    //loop through, calling kendindex to kstart, stopping at where a value != 0. have to call k fnctions then. easier way?
+    int endB = sizeof(b->array->positionIndex) / sizeof(b->array->positionIndex[0]) - 1;
+    if (b->array->positionIndex[endB] == 0){
+        endB--;
+        return bsa_maxindex(b);
+    }
 }
 
 // Clears up all space used
 bool bsa_free(bsa* b){
-    free(b);
+    //if b has been allocated space (!= NULL), then clear. if == NULL return false? check driver.c
+    //first false
+    if (b->array == NULL || b->array->positionIndex == NULL){
+        printf("does not need to be freed!"); //remove after
+        return false;
+    }
+    else{
+        free(b->array->positionIndex);
+        free(b->array);
+        free(b);
+    }
     return true;
 }
 
@@ -155,4 +193,6 @@ void test(void){
     // free(testArray);
 
     test_krow();
+    testIstart();
+    testIend();
 }
--- BSA/Alloc/specific.h
+++ BSA/Alloc/specific.h
@@ -6,11 +6,13 @@ int kth_row(int index, int *k);
 int index_start(int k);
 int index_end(int index_start);
 void test_krow(void);
+void testIstart(void);
+void testIend(void);
 
 typedef struct BSA_Col{
     int* positionIndex; //the position of d within a row created 
 }BSA_Col;
 
-struct bsa {
-    BSA_Col* array; //whole bsa struct, what pnter-index within pointer of array
+struct bsa { //whole bsa structure
+    BSA_Col* array; //1D array of pointers
 };
