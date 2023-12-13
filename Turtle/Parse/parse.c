5527546219ba4f46713e564e3dcbc4837b54fc2b
mihirgandecha <mihirgandecha@outlook.com>
2023-12-13 17:09:12 +0000

    script try out

--- Turtle/Parse/parse.c
+++ Turtle/Parse/parse.c
@@ -1,51 +1,40 @@
 #include "parse.h"
 
-int main(void) {
+int main(void){
     Program* turtle = (Program*)calloc(1, sizeof(Program));
-    if (turtle == NULL){
+    if (!turtle){
         ERROR("Turtle failed to initialise!\n");
-        return false;
-    }
-    if (!TurtleInit(turtle)){
-        ERROR("Failed to Go to TurtleInit!\n");
-        return 1; 
-    }
-    test();
-    return 0;
-}
-
-bool TurtleInit(Program *turtle){
-    //Need to INIT FUNC: while there is a user input that starts with "START" -> calloc space
-    //Take the input
-    int input = 0;
-    printf("Enter Turtle Commands:\n");
-    //How can I free memory if there is no input?
-    while(scanf("%s", turtle->wds[input]) == 1 && input >= MAXCMND){
-        input++;
+        return 1;
     }
     if(!Start(turtle)){
         ERROR("Algorithm failed to start\n");
-        return false;
+        return 1;
     }
+    test();
     printf("Parsed OK\n");
-    return turtle;
+    free(turtle);
+    return 0;
 }
 
 bool Start(Program *turtle){
-    if(!strsame(turtle->wds[turtle->cw], "START")){
+    turtle->cw = 0;
+    printf("Enter Turtle Commands:\n");
+    while(scanf("%s", turtle->cmnd[turtle->cw]) == 1 && turtle->cw >= MAXCMND){
+        if(!strsame(turtle->cmnd[turtle->cw], "START")){
         ERROR("No 'START' statement!\n");
         return false;
+        }
+        turtle->cw++;
+        Inslst(turtle);
     }
-    turtle->cw += 1;
-    Inslst(turtle);
     return true;
 }
 
 bool Inslst(Program *turtle){
-    if(strsame(turtle->wds[turtle->cw], "END")){
+    if(strsame(turtle->cmnd[turtle->cw], "END")){
         return true;
     }
-    while(!strsame(turtle->wds[turtle->cw], "END")){
+    while(!strsame(turtle->cmnd[turtle->cw], "END")){
         if(!Ins(turtle)){
             ERROR("Program failed to pass to Instruction Function.\n");
             return false;
@@ -55,9 +44,8 @@ bool Inslst(Program *turtle){
     return true;
 }
 
-
 bool Ins(Program *turtle){
-    if (strncmp(turtle->wds[turtle->cw], "FORWARD", 7)){
+    if (strsame(turtle->cmnd[turtle->cw], "FORWARD")){
         turtle->cw += 1;
         if(!Fwd(turtle)){
             ERROR("Forward did not execute!");
@@ -65,63 +53,89 @@ bool Ins(Program *turtle){
         }
         return true;
     }
-    else if(strncmp(turtle->wds[turtle->cw], "RIGHT", 5)){
+    else if(strsame(turtle->cmnd[turtle->cw], "RIGHT")){
         turtle->cw += 1; //is this a double?
         if(!Rgt(turtle)){
             ERROR("Right did not execute!");
             return false;
-        }    
+        }
+        return true;    
+    }
+    else{
+        return Inslst(turtle);
     }
     ERROR("Instructions failed!");
     return false;
 }
-
+//also conducting horizontal/vertical?
 bool Fwd(Program *turtle){
-    double var; //temporary
-    if (sscanf(turtle->wds[turtle->cw], "%lf", &var) != 1){
+    double num;
+    if (!Num(turtle, &num)){
         ERROR("Invalid number after FORWARD");
         return false;
     }
-    //How does forward work??
+    // if (sscanf(turtle->cmnd[turtle->cw], "%ld", num) != 1) {
+    //     ERROR("Invalid number input!");
+    //     return false;
+    // }
+    // if (sscanf("%s", turtle->cmnd[turtle->cw]) && Num(num) != 1){
+    //     ERROR("Invalid number after FORWARD");
+    //     return false;
+    // }
+    //How do we actually make it go forward??
     return true;
 }
 
-bool Rgt(Program *turtle) {
-    double var;
-    if (sscanf(turtle->wds[turtle->cw], "%lf", &var) != 1) {
-        ERROR("Invalid number after RIGHT");
+bool Num(Program *turtle, double *Num){
+    if (sscanf(turtle->cmnd[turtle->cw], "%lf", Num) != 1) {
+        ERROR("Invalid number input!");
         return false;
+    }
+    return true;
+}
+
+bool Rgt(Program *turtle){
+    double var;
+    if (sscanf(turtle->cmnd[turtle->cw], "%lf", &var) != 1){
+        ERROR("Invalid number after RIGHT");
+        return false;   
     }
     //how can we make go right? Conversion?
     return true;
 }
 
+
 void test(void){
     //Start Function Test;
-    Program testTurtle;
-    TurtleInit(&testTurtle);
-    strcpy(testTurtle.wds[0], "START");
-    testTurtle.cw = 0;
-    assert(Start(&testTurtle) == true);
+    Program* testTurtle = (Program*)calloc(1, sizeof(Program));
+    if (!testTurtle){
+        ERROR("Turtle Tests failed to initialise!\n");
+    }
+
+    strcpy(testTurtle->cmnd[0], "START");
+    testTurtle->cw = 0;
+    assert(Start(testTurtle) == true);
 
     //End Function Test:
-    strcpy(testTurtle.wds[1], "END");
-    testTurtle.cw = 1;
-    assert(Inslst(&testTurtle) == true);
-    assert(testTurtle.cw == 2);
+    strcpy(testTurtle->cmnd[1], "END");
+    testTurtle->cw = 1;
+    assert(Inslst(testTurtle) == true);
+    assert(testTurtle->cw == 1);
 
     //Testing Forward:
-    strcpy(testTurtle.wds[0], "FORWARD");
-    strcpy(testTurtle.wds[1], "10");
-    strcpy(testTurtle.wds[2], "END");
-    testTurtle.cw = 2; //or 3? 
-    assert(Ins(&testTurtle) == true);
+    strcpy(testTurtle->cmnd[0], "FORWARD");
+    strcpy(testTurtle->cmnd[1], "10");
+    strcpy(testTurtle->cmnd[2], "END");
+    testTurtle->cw = 2; //or 3? 
+    assert(Ins(testTurtle) == true);
 
     //Test for Right:
-    strcpy(testTurtle.wds[0], "RIGHT");
-    strcpy(testTurtle.wds[1], "90");
+    strcpy(testTurtle->cmnd[0], "RIGHT");
+    strcpy(testTurtle->cmnd[1], "90");
     //is it still End for 2?
-    testTurtle.cw = 2;
-    assert(Ins(&testTurtle) == true);
+    testTurtle->cw = 2;
+    assert(Ins(testTurtle) == true);
+
+    free(testTurtle);
 
 }
--- Turtle/Parse/parse.h
+++ Turtle/Parse/parse.h
@@ -6,23 +6,31 @@
 
 #define MAXCMND 100
 #define MAXTOKENSIZE 20
+#define MAXCOL 33
+#define MAXROW 51
 #define strsame(A,B) (strcmp(A, B)==0) //does this mean we are defaulting it to be false?
 #define ERROR(PHRASE) {fprintf(stderr, "Fatal Error %s occurred in %s, line %d\n", PHRASE, __FILE__, __LINE__); exit(EXIT_FAILURE);}
 
 
 struct prog{
-   char wds[MAXCMND][MAXTOKENSIZE]; //What is stored in each?
+   char cmnd[MAXCMND][MAXTOKENSIZE]; //What is stored in each?
    int cw; // Current Word
+   int size;
 };
 typedef struct prog Program;
 
-bool TurtleInit(Program *turtle);
+// struct board{
+//    int b[MAXCOL][MAXROW];
+// };
+// typedef struct board Board;
+
+// bool TurtleInit(Program *turtle);
 bool Start(Program *turtle);
 bool Inslst(Program *turtle);
 bool Ins(Program *turtle);
 bool Fwd(Program *turtle);
 bool Rgt(Program *turtle);
-// int Num(Program *turtle);
+bool Num(Program *turtle, double *Num);
 
 void test(void);
 
