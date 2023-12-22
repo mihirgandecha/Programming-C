e6c1a90960e72108bb83030df7c759268255f699
mihirgandecha <mihirgandecha@outlook.com>
2023-12-22 16:56:09 +0000

    teaching

---
+++ sahil/sahil.java
@@ -0,0 +1,13 @@
+class Person{
+    int age;
+    String name;
+public static void main(String[] args){
+
+}
+
+public int void x(int a, int b){
+    return(a+b);
+    age=5
+}
+
+}
---
+++ sahil/sahil.py
@@ -0,0 +1,5 @@
+x=[]
+print(x)
+x.append(5)
+x.append("Sahil")
+print(x)
